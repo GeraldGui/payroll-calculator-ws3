223d92e0f080b8fae1256df7276a21ac64de8b9c
Gerald G <geraldguignard2@gmail.com>
2026-04-21 09:16:13 -0400

    Finished adding the for loop to print the ID, Name, and Pay. also made the array and while loop to put split them into tokens and store then them to be used.

---
+++ src/main/java/com/pluralsight/Employee.java
@@ -0,0 +1,50 @@
+package com.pluralsight;
+
+public class Employee {
+    private int employeeId;
+    private double hoursWorked;
+    private double payRate;
+
+    private String name;
+
+    public Employee(int employeeId, String name, double hoursWorked, double payRate) {
+        this.employeeId = employeeId;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+        this.name = name;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getGrossPay() {
+        return hoursWorked * payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
---
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -0,0 +1,38 @@
+package com.pluralsight;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PayrollCalculator {
+    public static void main(String[] args) {
+
+        List<Employee> employees = new ArrayList<>();
+
+        String line;
+
+        try {
+            FileReader fileReader = new FileReader("employees.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while ((line = bufferedReader.readLine()) != null) {
+
+                String[] tokens = line.split("\\|");
+
+                int employeeId = Integer.parseInt(tokens[0]);
+                String name = tokens[1];
+                double hoursWorked = Double.parseDouble(tokens[2]);
+                double payRate = Double.parseDouble(tokens[3]);
+
+                employees.add(new Employee(employeeId, name, hoursWorked, payRate));
+
+            }
+            for (Employee makeEmployee : employees) {
+                System.out.printf("Employee ID: " + makeEmployee.getEmployeeId() + " \nName: " + makeEmployee.getName() + "\nPay: " + makeEmployee.getGrossPay() + "\n------------------------\n");
+            }
+        } catch (Exception ex) {
+            System.out.println("Wrong Input!");
+        }
+    }
+}
